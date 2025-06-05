76bbd14da7bd52c3dcf40c779333f407d42f64f6
Annareddyvineela12 <annareddyvineela12@gmail.com>
2025-06-05 15:38:07 +0530

    Update MaxHeapExample.java

--- MaxHeapExample.java
+++ MaxHeapExample.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class MaxHeapExample{
 public static void main(String[] args){
-PriorityQueue<Integer> maxHeap=new PriorityQueue<>();
+PriorityQueue<Integer> maxHeap=new PriorityQueue<>(Collections.reverseOrder();
 maxHeap.add(10);
 maxHeap.add(5);
 maxHeap.add(20);
